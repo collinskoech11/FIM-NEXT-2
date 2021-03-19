ef26d8c198f26ee1624cc89137b0b92245379326
collinskoech11 <collinskoechck34@gmail.com>
2021-03-20 00:34:46 +0300

    app update

--- fim-next/components/NavPage.js
+++ fim-next/components/NavPage.js
@@ -1,15 +1,28 @@
 import React from 'react'
-import {Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap'
+import {Navbar, Nav, Form, FormControl, Button, Dropdown } from 'react-bootstrap'
 
 const NavPage = () => {
     return (
         <>
             <Navbar bg="primary" variant="dark">
-                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                <Navbar.Brand href="#home">Faith International Ministries</Navbar.Brand>
                 <Nav className="mr-auto">
                 <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#features">Features</Nav.Link>
-                <Nav.Link href="#pricing">Pricing</Nav.Link>
+                <Nav.Link href="#features">Live Service</Nav.Link>
+                <Dropdown>
+                    <Dropdown.Toggle variant="primary" id="dropdown-basic">
+                        Our Branches
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu>
+                        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+                <Nav.Link href="#features">Just Fm</Nav.Link>
+                <Nav.Link href="#features">About Us </Nav.Link>
+                <Nav.Link href="#features">Help</Nav.Link>
                 </Nav>
                 <Form inline>
                 <FormControl type="text" placeholder="Search" className="mr-sm-2" />
