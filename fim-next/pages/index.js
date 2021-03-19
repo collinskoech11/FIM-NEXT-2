a2cb5874c7e9628c99d2e9644ccf6719e39c9fa3
collinskoech11 <collinskoechck34@gmail.com>
2021-03-20 00:22:57 +0300

    update

--- fim-next/pages/index.js
+++ fim-next/pages/index.js
@@ -1,9 +1,11 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import NavPage from '../components/NavPage'
 
 export default function Home() {
   return (
-   
-    
+    <>
+      <NavPage/>
+    </>
   )
 }
