fe2c6e59f0498629f10d89e6216849cdf029d6e4
collinskoech11 <collinskoechck34@gmail.com>
2021-03-20 18:55:31 +0300

    update page routes

---
+++ fim-next/pages/LiveService.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavPage from '../components/NavPage'
+import FooterPage from '../components/FooterPage'
+
+const LiveService = () => {
+    return (
+        <div>
+            <NavPage/>
+            <h1>THis is the Live service page</h1>
+            <FooterPage/>
+        </div>
+    )
+}
+
+export default LiveService
