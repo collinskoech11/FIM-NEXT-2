77ac88df48233aab7745ed5b6392d3b45541f76f
collinskoech11 <collinskoechck34@gmail.com>
2021-03-22 08:18:06 +0300

    update

---
+++ fim-next/components/LiveLink.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const LiveLink = () => {
+    return (
+        <div className="live-container">
+            <div className="live-text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium quod maxime voluptatibus, possimus fugit pariatur quam, a tenetur totam saepe quia officiis ullam, fugiat natus facere. Sit accusamus asperiores nesciunt.
+            </div>
+            <div className="live-image">
+                <img
+                    src="https://source.unsplash.com/1000x200/?android"
+                    width="150px"
+                    height="150px"
+                    alt="Live SErvice LInk"
+                />
+            </div>
+        </div>
+    )
+}
+
+export default LiveLink
--- fim-next/pages/index.js
+++ fim-next/pages/index.js
@@ -3,6 +3,7 @@ import styles from '../styles/Home.module.css'
 import NavPage from '../components/NavPage'
 import CarouselPage from '../components/CarouselPage'
 import FooterPage from '../components/FooterPage'
+import LiveLink from '../components/LiveLink'
 
 export default function Home() {
   return (
@@ -12,6 +13,21 @@ export default function Home() {
         <div className="caousel-container">
           <CarouselPage/>
         </div>
+      </div>
+      <div className="section">
+          <div className="img-container">
+            <img src="https://source.unsplash.com/1000x200/?android"
+            alt="Picture of the author"
+            width={400}
+            height={300}
+            />
+          </div>
+          <div className="text-container">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis magnam, aliquid mollitia blanditiis laborum ratione fuga rerum natus consequuntur reprehenderit quasi nihil voluptatum, a sed ut aliquam commodi ipsa modi.
+          </div>
+      </div>
+      <div className="section">
+        <LiveLink/>
       </div>
       <div className="section">
           <div className="img-container">
