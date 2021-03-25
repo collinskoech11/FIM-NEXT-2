2bdb3b831bcd6874e8eff60784a7f7e1650c88dd
collinskoech11 <collinskoechck34@gmail.com>
2021-03-25 16:52:03 +0300

    update styles

--- fim-next/components/LiveLink.js
+++ fim-next/components/LiveLink.js
@@ -4,10 +4,11 @@ const LiveLink = () => {
     return (
         <div className="live-container">
             <div className="live-text">
+                <h2>Join us Sunday mornings as we gather online</h2>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium quod maxime voluptatibus, possimus fugit pariatur quam, a tenetur totam saepe quia officiis ullam, fugiat natus facere. Sit accusamus asperiores nesciunt.
             </div>
             <div className="live-image">
-                <button className="live-btn">Check Out our Live Church Services</button>
+                <a href="/LiveService"></a><button className="live-btn">Check Out our Live Church Services</button>
             </div>
         </div>
     )
--- fim-next/pages/index.js
+++ fim-next/pages/index.js
@@ -16,13 +16,15 @@ export default function Home() {
       </div>
       <div className="section">
           <div className="img-container">
-            <img src="https://source.unsplash.com/1000x200/?android"
+            <img src="https://source.unsplash.com/1000x500/?android"
             alt="Picture of the author"
             width={400}
-            height={300}
+            height={500}
             />
           </div>
           <div className="text-container">
+            <h3>Word from our spiritual Father</h3>
+            <h2>Bishop Jimmy Bett</h2>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis magnam, aliquid mollitia blanditiis laborum ratione fuga rerum natus consequuntur reprehenderit quasi nihil voluptatum, a sed ut aliquam commodi ipsa modi.
           </div>
       </div>
@@ -34,10 +36,12 @@ export default function Home() {
             <img src="https://source.unsplash.com/1000x200/?android"
             alt="Picture of the author"
             width={400}
-            height={300}
+            height={350}
             />
           </div>
           <div className="text-container">
+            <h3>Our chairperson shares the vision of our church</h3>
+            <h2>Mr Stanley Cheruiyot</h2>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis magnam, aliquid mollitia blanditiis laborum ratione fuga rerum natus consequuntur reprehenderit quasi nihil voluptatum, a sed ut aliquam commodi ipsa modi.
           </div>
       </div>
