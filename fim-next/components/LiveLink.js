8260adb96cf93fff32b8949d7663f234fc3d4b4e
collinskoech11 <collinskoechck34@gmail.com>
2021-03-25 11:52:05 +0300

    update

--- fim-next/components/LiveLink.js
+++ fim-next/components/LiveLink.js
@@ -7,12 +7,7 @@ const LiveLink = () => {
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium quod maxime voluptatibus, possimus fugit pariatur quam, a tenetur totam saepe quia officiis ullam, fugiat natus facere. Sit accusamus asperiores nesciunt.
             </div>
             <div className="live-image">
-                <img
-                    src="https://source.unsplash.com/1000x200/?android"
-                    width="150px"
-                    height="150px"
-                    alt="Live SErvice LInk"
-                />
+                <button className="live-btn">Check Out our Live Church Services</button>
             </div>
         </div>
     )
