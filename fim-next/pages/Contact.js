4a20d488e7a53b05a7a65f0de95b3c4029c87bbf
collinskoech11 <collinskoechck34@gmail.com>
2021-03-30 04:37:05 +0300

    installed reactstrap

--- fim-next/pages/Contact.js
+++ fim-next/pages/Contact.js
@@ -1,131 +1,151 @@
-/** Components */
-const Card = props => (
-    <div className="card">
-      {/*<div className="waves">
-      </div>*/}
-      {props.children}
-    </div>
+import React from "react";
+
+// reactstrap components
+import { FormGroup, Form , Input } from "reactstrap";
+
+function Contact() {
+  return (
+    <>
+      <Form>
+        <FormGroup>
+          <label className=" form-control-label" htmlFor="example-text-input">
+            Text
+          </label>
+          <Input
+            defaultValue="John Snow"
+            id="example-text-input"
+            type="text"
+          ></Input>
+        </FormGroup>
+        <FormGroup>
+          <label className=" form-control-label" htmlFor="example-search-input">
+            Search
+          </label>
+          <Input
+            defaultValue="Tell me your secret ..."
+            id="example-search-input"
+            type="search"
+          ></Input>
+        </FormGroup>
+        <FormGroup>
+          <label className=" form-control-label" htmlFor="example-email-input">
+            Email
+          </label>
+          <Input
+            defaultValue="@example.com"
+            id="example-email-input"
+            type="email"
+          ></Input>
+        </FormGroup>
+        <FormGroup>
+          <label className=" form-control-label" htmlFor="example-url-input">
+            URL
+          </label>
+          <Input
+            defaultValue=""
+            id="example-url-input"
+            type="url"
+          ></Input>
+        </FormGroup>
+        <FormGroup>
+          <label className=" form-control-label" htmlFor="example-tel-input">
+            Phone
+          </label>
+          <Input
+            defaultValue="40-(770)-888-444"
+            id="example-tel-input"
+            type="tel"
+          ></Input>
+        </FormGroup>
+        <FormGroup>
+          <label
+            className=" form-control-label"
+            htmlFor="example-password-input"
+          >
+            Password
+          </label>
+          <Input
+            defaultValue="password"
+            id="example-password-input"
+            type="password"
+          ></Input>
+        </FormGroup>
+        <FormGroup>
+          <label className=" form-control-label" htmlFor="example-number-input">
+            Number
+          </label>
+          <Input
+            defaultValue="23"
+            id="example-number-input"
+            type="number"
+          ></Input>
+        </FormGroup>
+        <FormGroup>
+          <label
+            className=" form-control-label"
+            htmlFor="example-datetime-local-input"
+          >
+            Datetime
+          </label>
+          <Input
+            defaultValue="2018-11-23T10:30:00"
+            id="example-datetime-local-input"
+            type="datetime-local"
+          ></Input>
+        </FormGroup>
+        <FormGroup>
+          <label className=" form-control-label" htmlFor="example-date-input">
+            Date
+          </label>
+          <Input
+            defaultValue="2018-11-23"
+            id="example-date-input"
+            type="date"
+          ></Input>
+        </FormGroup>
+        <FormGroup>
+          <label className=" form-control-label" htmlFor="example-month-input">
+            Month
+          </label>
+          <Input
+            defaultValue="2018-11"
+            id="example-month-input"
+            type="month"
+          ></Input>
+        </FormGroup>
+        <FormGroup>
+          <label className=" form-control-label" htmlFor="example-week-input">
+            Week
+          </label>
+          <Input
+            defaultValue="2018-W23"
+            id="example-week-input"
+            type="week"
+          ></Input>
+        </FormGroup>
+        <FormGroup>
+          <label className=" form-control-label" htmlFor="example-time-input">
+            Time
+          </label>
+          <Input
+            defaultValue="10:30:00"
+            id="example-time-input"
+            type="time"
+          ></Input>
+        </FormGroup>
+        <FormGroup>
+          <label className=" form-control-label" htmlFor="example-color-input">
+            Color
+          </label>
+          <Input
+            defaultValue="#5e72e4"
+            id="example-color-input"
+            type="color"
+          ></Input>
+        </FormGroup>
+      </Form>
+    </>
   );
-  
-  const Form = props => (
-    <form className="form">{props.children}</form>
-  );
-  
-  const TextInput = props => (
-    <div
-      className="text-input">
-      <label
-        className={(props.focus || props.value !== '') ? 'label-focus' : ''}
-        htmlFor={props.name}>{props.label}</label>
-      <input
-        className={(props.focus || props.value !== '') ? 'input-focus' : ''}
-        type="text"
-        name={props.name}
-        value={props.value}
-        onChange={props.onChange}
-        onInput={props.onInput}
-        onFocus={props.onFocus}
-        onBlur={props.onBlur} />
-    </div>
-  );
-  
-  const TextArea = props => (
-    <div
-      className="text-area">
-      <label
-        className={(props.focus || props.value !== '') ? 'label-focus' : ''}
-        htmlFor={props.name}>{props.label}</label>
-      <textarea
-        className={(props.focus || props.value !== '') ? 'input-focus' : ''}
-        name={props.name}
-        value={props.value}
-        onChange={props.onChange}
-        onInput={props.onInput}
-        onFocus={props.onFocus}
-        onBlur={props.onBlur} />
-    </div>
-  );
-  
-  const Button = props => (
-    <button
-      className="button">{props.children}</button>
-  );
-  
-  /** Root Component */
-  class App extends React.Component {
-    constructor() {
-      super();
-      this.state = {
-        name: {
-          name: 'name',
-          label: 'Name',
-          value: '',
-          focus: false,
-        },
-        email: {
-          name: 'email',
-          label: 'Email',
-          value: '',
-          focus: false,
-        },
-        message: {
-          name: 'message',
-          label: 'Message',
-          value: '',
-          focus: false,
-        },
-      }
-    }
-    
-    handleFocus(e) {
-      const name = e.target.name;
-      const state = Object.assign({}, this.state[name]);
-      state.focus = true;
-      this.setState({ [name]: state },()=>{console.log(state)});
-    }
-    
-    handleBlur(e) {
-      const name = e.target.name;
-      const state = Object.assign({}, this.state[name]);
-      state.focus = false;
-      this.setState({ [name]: state },()=>{console.log(state)});
-    }
-    
-    handleChange(e) {
-      const name = e.target.name;
-      const state = Object.assign({}, this.state[name]);
-      state.value = e.target.value;
-      this.setState({ [name]: state },()=>{console.log(state)});
-    }
-    
-    render() {
-      const {name, email, message} = this.state;
-      return (
-        <div className="containeroo">
-          <Card>
-            <h1>Send us a Message!</h1>
-            <Form>
-              <TextInput
-                {...name}
-                onFocus={this.handleFocus.bind(this)}
-                onBlur={this.handleBlur.bind(this)}
-                onChange={this.handleChange.bind(this)} />
-              <TextInput
-                {...email}
-                onFocus={this.handleFocus.bind(this)}
-                onBlur={this.handleBlur.bind(this)}
-                onChange={this.handleChange.bind(this)} />
-              <TextArea
-                {...message}
-                onFocus={this.handleFocus.bind(this)}
-                onBlur={this.handleBlur.bind(this)}
-                onChange={this.handleChange.bind(this)} />
-              <Button>Send</Button>
-            </Form>
-          </Card>
-        </div>
-      );
-    }
-  }
-  
-export default Contact
+}
+
+export default Contact;
