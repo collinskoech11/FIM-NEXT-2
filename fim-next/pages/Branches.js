764b570b3f3953ceaff1625d27a8e4cb53eb1a1e
collinskoech11 <collinskoechck34@gmail.com>
2021-04-08 19:33:59 +0300

    update

--- fim-next/pages/Branches.js
+++ fim-next/pages/Branches.js
@@ -6,7 +6,31 @@ const Branches = () => {
     return (
         <>
             <NavPage/>
-                <h1>This is the branches page</h1>
+            <div className="profile-card-4 text-center"><img src="http://envato.jayasankarkr.in/code/profile/assets/img/profile-4.jpg" className="img img-responsive" />
+                <div className="profile-content">
+                    <div className="profile-name">John Doe
+                    <p>@johndoedesigner</p>
+                    </div>
+                    <div className="profile-description">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor.</div>
+                    <div className="row">
+                    <div className="col-xs-4">
+                        <div className="profile-overview">
+                        <p>TWEETS</p>
+                        <h4>1300</h4></div>
+                    </div>
+                    <div className="col-xs-4">
+                        <div className="profile-overview">
+                        <p>FOLLOWERS</p>
+                        <h4>250</h4></div>
+                    </div>
+                    <div className="col-xs-4">
+                        <div className="profile-overview">
+                        <p>FOLLOWING</p>
+                        <h4>168</h4></div>
+                    </div>
+                    </div>
+                </div>
+                </div>
             <FooterPage/>
         </>
     )
