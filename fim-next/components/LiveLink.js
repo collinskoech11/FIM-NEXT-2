4b18796519c64008851d0f055282ea6c267da94a
collinskoech11 <collinskoechck34@gmail.com>
2021-04-08 17:17:15 +0300

    update copntent

--- fim-next/components/LiveLink.js
+++ fim-next/components/LiveLink.js
@@ -5,7 +5,7 @@ const LiveLink = () => {
         <div className="live-container">
             <div className="live-text">
                 <h2>Join us Sunday mornings as we gather online</h2>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium quod maxime voluptatibus, possimus fugit pariatur quam, a tenetur totam saepe quia officiis ullam, fugiat natus facere. Sit accusamus asperiores nesciunt.
+                The Faith International Ministries Church wants to do everything we can to ensure that our communities are taking the necessary precautions to slow the spread of COVID-19. At the same time, we want to make it possible for everyone to experience the fellowship and Biblical teaching that keeps our faith strong, which is more important now than ever. With this in mind, we are ready to offer a series of special live stream worship services, Sundays at 11am GMT+3. These will take the place of in-person services for all of our Bay Area locations. We hope you’ll join us online!
             </div>
             <div className="live-image">
                 <a href="/LiveService"></a><button className="live-btn">Check Out our Live Church Services</button>
