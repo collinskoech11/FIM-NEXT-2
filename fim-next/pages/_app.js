0407bb915212498714e0e6de91241fe7c17e954d
collinskoech11 <collinskoechck34@gmail.com>
2021-04-08 19:11:58 +0300

    update

--- fim-next/pages/_app.js
+++ fim-next/pages/_app.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/css/bootstrap.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps}/>
 }
 
 export default MyApp
