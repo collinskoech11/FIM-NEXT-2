38068697371db617001cfc548ddab11fd0e8f89b
collinskoech11 <collinskoechck34@gmail.com>
2021-04-09 18:07:52 +0300

    update

--- fim-next/pages/Satelite.js
+++ fim-next/pages/Satelite.js
@@ -2,33 +2,101 @@ import React from 'react'
 
 const Satelite = () => {
     return (
-        <div>
-            <div className="profile-card-4 text-center"><img src="http://envato.jayasankarkr.in/code/profile/assets/img/profile-4.jpg" className="img img-responsive" />
-                <div className="profile-content">
-                    <div className="profile-name">John Doe
-                    <p>@johndoedesigner</p>
-                    </div>
-                    <div className="profile-description">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor.</div>
-                    <div className="row">
-                    <div className="col-xs-4">
-                        <div className="profile-overview">
-                        <p>TWEETS</p>
-                        <h4>1300</h4></div>
-                    </div>
-                    <div className="col-xs-4">
-                        <div className="profile-overview">
-                        <p>FOLLOWERS</p>
-                        <h4>250</h4></div>
-                    </div>
-                    <div className="col-xs-4">
-                        <div className="profile-overview">
-                        <p>FOLLOWING</p>
-                        <h4>168</h4></div>
-                    </div>
-                    </div>
-                </div>
-</div>
-        </div>
+        <><Card bg="primary" text="white" style={{ width: '18rem' }}>
+        <Card.Header>Header</Card.Header>
+        <Card.Body>
+          <Card.Title>Primary Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the bulk
+            of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <br />
+    
+      <Card bg="secondary" text="white" style={{ width: '18rem' }}>
+        <Card.Header>Header</Card.Header>
+        <Card.Body>
+          <Card.Title>Secondary Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the bulk
+            of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <br />
+    
+      <Card bg="success" text="white" style={{ width: '18rem' }}>
+        <Card.Header>Header</Card.Header>
+        <Card.Body>
+          <Card.Title>Success Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the bulk
+            of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <br />
+    
+      <Card bg="danger" text="white" style={{ width: '18rem' }}>
+        <Card.Header>Header</Card.Header>
+        <Card.Body>
+          <Card.Title>Danger Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the bulk
+            of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <br />
+    
+      <Card bg="warning" text="white" style={{ width: '18rem' }}>
+        <Card.Header>Header</Card.Header>
+        <Card.Body>
+          <Card.Title>Warning Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the bulk
+            of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <br />
+    
+      <Card bg="info" text="white" style={{ width: '18rem' }}>
+        <Card.Header>Header</Card.Header>
+        <Card.Body>
+          <Card.Title>Info Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the bulk
+            of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <br />
+    
+      <Card bg="dark" text="white" style={{ width: '18rem' }}>
+        <Card.Header>Header</Card.Header>
+        <Card.Body>
+          <Card.Title>Dark Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the bulk
+            of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <br />
+    
+      <Card bg="light" style={{ width: '18rem' }}>
+        <Card.Header>Header</Card.Header>
+        <Card.Body>
+          <Card.Title>Light Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the bulk
+            of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <br /></>
     )
 }
 
